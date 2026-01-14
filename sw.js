d63a169889b1b3e2ea4ee0ebc2ed0fc6fbcf62a1
gamadasilvaeduardo-pixel <gamadasilvaeduardo@gmail.com>
2026-01-13 23:34:19 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "validador-tags-v1";
+const CACHE = "validador-tags-v2";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
